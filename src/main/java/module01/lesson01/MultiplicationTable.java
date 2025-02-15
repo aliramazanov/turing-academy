9f671445ad91dbb820e10e56b83827f76fbff754
Ali Ramazanov <toaliramazanov@gmail.com>
2025-02-15 20:46:26 +0400

    rename class name on lesson 01

--- src/main/java/module01/lesson01/MultiplicationTable.java
+++ src/main/java/module01/lesson01/MultiplicationTable.java
@@ -1,6 +1,6 @@
 package main.java.module01.lesson01;
 
-public class Multiplication {
+public class MultiplicationTable {
     public static void main(String[] args) {
         for (int i = 2; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
