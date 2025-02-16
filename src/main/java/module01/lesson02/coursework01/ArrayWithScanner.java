b42ce06a51316ede5dcb8941aa74de6c7a01ea91
Ali Ramazanov <toaliramazanov@gmail.com>
2025-02-16 19:50:17 +0400

    refactor scanner

--- src/main/java/module01/lesson02/coursework01/ArrayWithScanner.java
+++ src/main/java/module01/lesson02/coursework01/ArrayWithScanner.java
@@ -6,10 +6,13 @@ public class ArrayWithScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+
         int [] arr = new int[n];
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
+
         System.out.println(Arrays.toString(arr));
     }
 }
