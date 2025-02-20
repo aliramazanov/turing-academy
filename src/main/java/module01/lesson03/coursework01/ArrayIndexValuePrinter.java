2db464392756f5ae72aaac2240e3e78f4971815a
Ali Ramazanov <toaliramazanov@gmail.com>
2025-02-20 20:50:53 +0400

    fix naming to comply with convention

--- src/main/java/module01/lesson03/coursework01/ArrayIndexValuePrinter.java
+++ src/main/java/module01/lesson03/coursework01/ArrayIndexValuePrinter.java
@@ -1,12 +1,12 @@
 package main.java.module01.lesson03.coursework01;
 
-public class PrintArrayWithIndexAndValue {
+public class ArrayIndexValuePrinter {
 
     public static void main(String[] args) {
-        print(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        printArrayElements(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
     }
 
-    public static void print(int[] arr) {
+    public static void printArrayElements(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int value = arr[i];
             System.out.println("Index: " + i + " Value: " + value);
