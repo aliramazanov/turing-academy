7077a5008ae4af310cf2643e75b3a03933d9f4cc
Ali Ramazanov <toaliramazanov@gmail.com>
2025-02-27 17:17:23 +0400

    add second task week planner for module one

--- src/module01/problemsolvingtasks/task02/ShipBattle.java
+++ src/module01/problemsolvingtasks/task02/ShipBattle.java
@@ -1,15 +1,47 @@
 package module01.problemsolvingtasks.task02;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class ShipBattle {
     public static Scanner sc = new Scanner(System.in);
     public static boolean isGameOver = false;
+    public static int bound = 5;
 
     public static void main(String[] args) {
         welcomePlayer();
         String name = getPlayerName();
 
+        char[][] field = createField();
+
+        int targetRow = createTarget();
+        int targetCol = createTarget();
+
+        while (!isGameOver) {
+            displayGameField(field);
+
+            int rowIndex = getInput("row");
+            int colIndex = getInput("column");
+
+            if (field[rowIndex][colIndex] != '-') {
+                System.out.println("Try another square");
+                continue;
+            }
+
+
+            boolean isHit = (rowIndex == targetRow && colIndex == targetCol);
+
+            if (isHit) {
+                field[rowIndex][colIndex] = 'x';
+                displayGameField(field);
+                System.out.println(name + ", you have won!");
+                isGameOver = true;
+            } else {
+                field[rowIndex][colIndex] = '*';
+            }
+        }
+
+        sc.close();
     }
 
     public static void welcomePlayer() {
@@ -26,15 +58,52 @@ public class ShipBattle {
     }
 
     public static char[][] createField() {
-        short size = 5;
-        char[][] field = new char[size][size];
+        char[][] field = new char[bound][bound];
 
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                field[row][col] = '-';
+        for (int i = 0; i < bound; i++) {
+            for (int j = 0; j < bound; j++) {
+                field[i][j] = '-';
             }
         }
 
         return field;
+    }
+
+    public static void displayGameField(char[][] field) {
+        System.out.println("- 1 2 3 4 5");
+
+        for (int i = 0; i < field.length; i++) {
+            System.out.print((i + 1) + " ");
+
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static int createTarget() {
+        Random random = new Random();
+        return random.nextInt(bound);
+    }
+
+    public static int getInput(String type) {
+        while (true) {
+            System.out.print("Enter the field " + type + " (1 to 5): ");
+
+            if (sc.hasNextInt()) {
+                int input = sc.nextInt();
+
+                if (input >= 1 && input <= bound) {
+                    return input - 1;
+                } else {
+                    System.out.println("Please enter a number in range " + type + ": (1 to 5)");
+                }
+            } else {
+                String invalidInput = sc.next();
+                System.out.println("Please enter a number. This is not a number => " + invalidInput);
+            }
+        }
     }
 }
--- src/module01/problemsolvingtasks/task03/WeekPlanner.java
+++ src/module01/problemsolvingtasks/task03/WeekPlanner.java
@@ -3,30 +3,55 @@ package module01.problemsolvingtasks.task03;
 import java.util.Scanner;
 
 public class WeekPlanner {
-    public static void main(String[] args) {
-        String[][] schedule = new String[7][2];
+    public static String[][] schedule = new String[7][2];
+    public static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        initializeSchedule();
+        run();
+    }
+
+    public static void initializeSchedule() {
         schedule[0][0] = "Monday";
-        schedule[0][1] = "go to courses;";
+        schedule[0][1] = "go to the police station";
         schedule[1][0] = "Tuesday";
-        schedule[1][1] = "go to the police station";
+        schedule[1][1] = "go to the gym";
         schedule[2][0] = "Wednesday";
         schedule[2][1] = "meet with friends";
         schedule[3][0] = "Thursday";
-        schedule[3][1] = "watch a film";
+        schedule[3][1] = "read a book";
         schedule[4][0] = "Friday";
-        schedule[4][1] = "play games";
+        schedule[4][1] = "do leftover tasks";
         schedule[5][0] = "Saturday";
-        schedule[5][1] = "go to bowling";
+        schedule[5][1] = "plan next week";
         schedule[6][0] = "Sunday";
-        schedule[6][1] = "do home work";
-
+        schedule[6][1] = "chill out";
     }
 
-    public static void print(String[][] schedule, String day) {
-    }
+    public static void run() {
+        while (true) {
+            System.out.println("Write the day: ");
+            String input = scanner.nextLine().trim().toLowerCase();
+            boolean isFound = false;
 
-    static void change(String[][] schedule, String day, Scanner scanner) {
-    }
+            if (input.equals("exit")) {
+                break;
+            }
 
+            for (int i = 0; i < schedule.length; i++) {
+                String day = schedule[i][0].toLowerCase();
+                String task = schedule[i][1];
+
+                if (day.equals(input)) {
+                    System.out.println(day + ": " + task + ".");
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (!isFound) {
+                System.out.println("Please try again.");
+            }
+        }
+    }
 }
